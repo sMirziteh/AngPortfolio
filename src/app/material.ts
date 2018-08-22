323346c9e8c9063e5826f10420e39931bc8d6b4a
smirziteh <shelby.mirziteh@gmail.com>
2018-08-21 17:30:38 -0700

    played with colors and progress bars

--- src/app/material.ts
+++ src/app/material.ts
@@ -5,13 +5,15 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
 
 
 
 
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule,MatToolbarModule, MatMenuModule, MatGridListModule,MatTabsModule],
-  exports: [MatButtonModule, MatCheckboxModule,MatToolbarModule, MatMenuModule, MatGridListModule,MatTabsModule],
+  imports: [MatButtonModule, MatCheckboxModule,MatToolbarModule, MatMenuModule, MatGridListModule,MatTabsModule, MatProgressBarModule],
+  exports: [MatButtonModule, MatCheckboxModule,MatToolbarModule, MatMenuModule, MatGridListModule,MatTabsModule, MatProgressBarModule],
 })
 export class MaterialModule { }
