84887e22e5e05ce0e2cfc08c809ce01075dc1102
shelby mirziteh <smirziteh@localhost.localdomain>
2018-09-12 14:13:18 -0700

    finished history component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,13 @@ import { TskillsComponent } from './tskills/tskills.component';
 import { EduComponent } from './edu/edu.component';
 import { AwardsComponent } from './awards/awards.component';
 import { ExpComponent } from './exp/exp.component';
-import { SkillsComponent } from './skills/skills.component'
+import { SkillsComponent } from './skills/skills.component';
+import { BioComponent } from './bio/bio.component';
+import { PicsComponent } from './pics/pics.component';
+import { HistoryComponent } from './history/history.component';
+import { IntrestsComponent } from './intrests/intrests.component';
+import { GoalsComponent } from './goals/goals.component';
+import { ReadingComponent } from './reading/reading.component'
 
 @NgModule({
   declarations: [
@@ -27,7 +33,13 @@ import { SkillsComponent } from './skills/skills.component'
     EduComponent,
     AwardsComponent,
     ExpComponent,
-    SkillsComponent
+    SkillsComponent,
+    BioComponent,
+    PicsComponent,
+    HistoryComponent,
+    IntrestsComponent,
+    GoalsComponent,
+    ReadingComponent
   ],
   imports: [
     BrowserModule,
